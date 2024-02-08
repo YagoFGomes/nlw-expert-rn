138c5967f1e7e3a9675e0c2c48b941105b356832
Yago Ferreira <yago.fgomes@gmail.com>
2024-02-08 12:48:29 +0000

    adicionando a persistencia de dados no dispositivo com AsyncStorage

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,5 +1,9 @@
 import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
+
+import { createJSONStorage, persist } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import * as cartInMemory from "./helpers/cart-in-memory";
 
 export interface ProductCartProps extends ProductProps {
@@ -11,15 +15,23 @@ interface StateProps {
     remove: (productId: string) => void;
 }
 
-export const useCardStore = create<StateProps>((set) => ({
-    products: [],
-    add: (product: ProductProps) =>
-        set((state) => ({
-            products: cartInMemory.add(state.products, product),
-        })),
+export const useCardStore = create(
+    persist<StateProps>(
+        (set) => ({
+            products: [],
+            add: (product: ProductProps) =>
+                set((state) => ({
+                    products: cartInMemory.add(state.products, product),
+                })),
 
-    remove: (productId: string) =>
-        set((state) => ({
-            products: cartInMemory.remove(state.products, productId),
-        })),
-}));
+            remove: (productId: string) =>
+                set((state) => ({
+                    products: cartInMemory.remove(state.products, productId),
+                })),
+        }),
+        {
+            name: "nlw-expert:cart",
+            storage: createJSONStorage(() => AsyncStorage),
+        }
+    )
+);
